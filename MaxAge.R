e4b700aedc358e2ffe592718d4ca7eecc72812d2
torrem <torrem27@gmail.com>
2019-10-29 09:47:09 -0600

    New Functions

---
+++ MaxAge.R
@@ -0,0 +1,86 @@
+# Max age of individuals
+#
+#'
+#'@title Take disMELS output file dfrs (from readAllResults) and produce hsitogram of max age
+#'
+#'@description Function to produce produce hist of max age
+#'
+#'@return hist that shows max age
+#'
+#'@export
+#'
+#'
+require(ggplot2)
+
+
+MaxAge <-function(group1, group2, group3,col1='green', col2='purple', col3='red'){
+
+  FreqTableX = data.frame(Breaks = seq(1,370,by=10))
+
+  for (i in 1:length(group1)){
+    resdr = group1[i]
+    load(paste(resdr,"/dfrs.RData",sep=""))
+    #if (length(dfrs) < 5) next
+    settlers = dfrs[[5]]
+    settlers = settlers[order(settlers$origID)[!duplicated(sort(settlers$origID))],] ## makes sure only unique settlers are used
+
+    settlers$age_in_days<- as.numeric(round(difftime(settlers$time,settlers$startTime , units = c("days"))))
+
+    hist = hist(settlers$age_in_days, breaks = seq(0,370, by=10), plot=FALSE)
+    counts = hist$counts
+    FreqTableX$counts = counts
+    FreqTableX$counts = (FreqTableX$counts/sum(FreqTableX$counts)) * 100
+
+    colnames(FreqTableX)[i+1] = paste(names(group1)[i])
+  }
+  FreqTableX$mean = apply(FreqTableX[,c(seq(2,length(group1)+1,by=1))],1, mean)
+
+
+
+FreqTableY = data.frame(Breaks = seq(1,370,by=10))
+
+for (i in 1:length(group2)){
+  resdr = group2[i]
+  load(paste(resdr,"/dfrs.RData",sep=""))
+  #if (length(dfrs) < 5) next
+  settlers = dfrs[[5]]
+  settlers = settlers[order(settlers$origID)[!duplicated(sort(settlers$origID))],] ## makes sure only unique settlers are used
+
+  settlers$age_in_days<- as.numeric(round(difftime(settlers$time,settlers$startTime , units = c("days"))))
+
+  hist = hist(settlers$age_in_days, breaks = seq(0,370, by=10), plot=FALSE)
+  counts = hist$counts
+  FreqTableY$counts = counts
+  FreqTableY$counts = (FreqTableY$counts/sum(FreqTableY$counts)) * 100
+
+  colnames(FreqTableY)[i+1] = paste(names(group2)[i])
+}
+FreqTableY$mean = apply(FreqTableY[,c(seq(2,length(group2)+1,by=1))],1, mean)
+
+
+
+
+FreqTableZ = data.frame(Breaks = seq(1,370,by=10))
+
+for (i in 1:length(group3)){
+  resdr = group3[i]
+  load(paste(resdr,"/dfrs.RData",sep=""))
+  #if (length(dfrs) < 5) next
+  settlers = dfrs[[5]]
+  settlers = settlers[order(settlers$origID)[!duplicated(sort(settlers$origID))],] ## makes sure only unique settlers are used
+
+  settlers$age_in_days<- as.numeric(round(difftime(settlers$time,settlers$startTime , units = c("days"))))
+
+  hist = hist(settlers$age_in_days, breaks = seq(0,370, by=10), plot=FALSE)
+  counts = hist$counts
+  FreqTableZ$counts = counts
+  FreqTableZ$counts = (FreqTableZ$counts/sum(FreqTableZ$counts)) * 100
+
+  colnames(FreqTableZ)[i+1] = paste(names(group3)[i])
+}
+FreqTableZ$mean = apply(FreqTableZ[,c(seq(2,length(group3)+1,by=1))],1, mean)
+
+
+
+
+}
---
+++ ReadList.R
@@ -0,0 +1,57 @@
+# Read in list of batch runs
+#
+#'
+#'@title Take raw disMELs resutls and produce dfrs list
+#'
+#'@description Function to produce dfrs list
+#'
+#'@return list of dfrs
+#'
+#'@export
+#'
+#'
+
+
+
+ReadList <-function(FoldDir){
+
+#### Read in lists---------------
+
+  Aresd = FoldDir
+
+    ## make A list##
+
+    Afold =  list.files (pattern = "^[:0-2:][0-9][0-9][0-9]", path = Aresd)
+
+    A = list()
+    for (i in 1:length(Afold)){
+      A[i] = paste(Aresd,"/",Afold[i],sep="")
+      names(A)[i]= paste('A',Afold[i], sep="")
+    }
+
+    ## check each batch run and get rid of Years with no settlers ##
+    rmlist = vector()
+    for ( i in 1:length(Afold)){
+      Fsize = file.info(paste(A[[i]],"/results.disMELS.IBMs.SnowCrab.ImmatureCrab.ImmatureFemale.csv", sep=""))$size
+      if (Fsize < 5000){
+        rmlist = c(rmlist,i)
+        print(paste("removing ", Afold[i], " due to low settler numbers" ,sep=""))
+      }
+
+    }
+    if(length(rmlist>0) ){A = A[-rmlist]}
+
+    ## make A dfrs for each item in list ##
+    for(i in 1:length(A)){
+      if (     ("dfrs.RData" %in% list.files(path = paste(A[i])))==TRUE          ){
+        print(paste(Afold[i], " has already been formatted", sep=""))
+      }
+      if (     ("dfrs.RData" %in% list.files(path = paste(A[i])))==FALSE          ){
+        print(paste("formatting ",Afold[i], sep=""))
+        readResults(resdr = A[i], resfn = 'results.' )
+      }
+
+    }
+return(A)
+
+}
