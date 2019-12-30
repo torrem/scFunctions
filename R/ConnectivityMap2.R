70d07ee9b7749dcc9a9c217722f0b573488f8c29
torrem <torrem27@gmail.com>
2019-12-30 10:38:36 -0700

    Add PNG output

--- R/ConnectivityMap2.R
+++ R/ConnectivityMap2.R
@@ -20,7 +20,7 @@
 
 
 
-ConnMap <-function(group){
+ConnMap <-function(group, path){
 
   info<-getLifeStageInfo.SnowCrab();
   typeNames<-factor(info$lifeStageTypes$typeName,levels=info$lifeStageTypes$typeName);#typeNames as factor levels
@@ -167,14 +167,8 @@ ConnectMatrix = apply(simplify2array(CMlist), 1:2, mean)
   ## Label Zones ##
 
 
-  # getBeringMap()
-  # lab = 1:27
-  # dd = c(200.05070, 196.80137, 193.84557, 192.25913, 191.10922, 197.80395, 195.25373, 191.68229, 189.12109, 187.36041, 195.03239, 193.23246, 189.45572, 186.0, 184.07891, 184.20322, 180.17429, 180.1170, 174.95373, 191.13708, 186.01163, 178.9745, 174.44257 , 182.2094, 177.50780, 174.65190, 194.98104)
-  # gg = c(57.50158,  58.07277,  59.20534,  60.28851,  61.68657,  56.14115,  56.82816,  57.87291,  59.05969,  60.88825,  54.95754,  55.63593,  56.56982,  58.0,  59.63626,  62.82903,  64.07788,  61.3,  61.52478,  54.65115,  56.19912,  60.0044,  60.83757,   55.0024,  57.65041,  59.49971,  53.94815)
-  # maptools::pointLabel(dd, gg,labels=paste(lab), cex=1.5, col="black")
 
-
-
+  png(paste(path,"/ConnectivityMap_",names(group)[kk],".png",sep=""), width = 12, height = 12, units = "in", res = 200)
   getBeringMap()
   lab = 1:27
   dd = c(200.05070, 196.80137, 193.84557, 192.25913, 191.10922, 197.80395, 195.25373, 191.68229, 189.12109, 187.36041, 195.03239, 193.23246, 189.45572, 186.0, 184.07891, 184.20322, 180.17429, 180.1170, 174.95373, 191.13708, 186.01163, 178.9745, 174.44257 , 182.2094, 177.50780, 174.65190, 194.98104)
@@ -217,7 +211,7 @@ ConnectMatrix = apply(simplify2array(CMlist), 1:2, mean)
       }
   }
  # }
-
+dev.off()
  # return(CMap)
 
 }
