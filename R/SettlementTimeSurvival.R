cc3b89f2737710ea6537afd96cf7737272b2235b
torrem <torrem27@gmail.com>
2020-06-08 13:51:19 -0600

    Survival funtion and time until settlement

--- R/SettlementTimeSurvival.R
+++ R/SettlementTimeSurvival.R
@@ -1,8 +1,56 @@
-IMDHist <-function(group, path){
+# Find time until settlement and percent survival
+#
+#'
+#'@title Take disMELS output file dfrs (from readAllResults) and produce histogram of IMD for each stage
+#'
+#'@description Function to produce produce frequency plot that shows time of settle
+#'
+#'@return frequency plot that shows time of settle
+#'
+#'@export
+#'
+#'
+#'
 
-  FreqTable = data.frame(year=numeric(), typeName=character(), origID=numeric(),ageInStage=numeric(), TempInStage=numeric())
+
+group=Fixed_Hindcast[25:34]
 
 
-  for (kk in 1:length(group)){
+
+
+SetSurv <-function(group){
+
+  TTS = data.frame(timeSet = vector())
+  Surv = data.frame(SP = rep(NA, length(group)))
+
+   for (kk in 1:length(group)){
     resdr = group[kk]
     load(paste(resdr,"/dfrs.RData",sep=""))
+
+
+
+
+    #### find starters and settlers ####_
+    starters = as.data.frame(dfrs[[1]])
+    starters$starter = ifelse(starters$startTime == starters$time, 1,0)
+    starters = subset(starters, starter==1)
+    #starters = starters[!duplicated(starters$origID), ]
+
+    settlers = dfrs[[4]]
+    #settlers = settlers[!duplicated(settlers$origID), ]
+    settlers = settlers[order(settlers$origID)[!duplicated(sort(settlers$origID))],] ## makes sure only unique settlers are used
+
+    Surv[kk,1] = (nrow(settlers)/nrow(starters))*100
+
+
+
+
+   }
+
+
+
+
+
+
+
+}
